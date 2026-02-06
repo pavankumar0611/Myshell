ddd9e205f4a48029a51ddf11329caec8d7d9b3bc
pavankumar0611 <pavankohli0644@gmail.com>
2026-02-05 19:30:07 -0800

    Added pipes feature into shell program.

--- running_exectubale.c
+++ running_exectubale.c
@@ -9,13 +9,105 @@
 #include <ctype.h>
 
 #define SIZE 100
+#define MAX_ARGS 10
+#define MAX_LEN  50
+#define MAX_CMDS 10
+
 
 void trim_trailing_space(char *s) {
 	int  len = strlen(s);
-	/* for removing railing spaces */
+	/* for removing trailing spaces */
 	while (len > 0 && isspace((unsigned char)s[len - 1])) {
 		s[--len] = '\0';
 	}   
+}
+
+//to check if the string contains the pipe 
+int contains_pipe(char *str) {
+	if (strchr ( str , '|') != NULL)
+		return 1;
+	else
+		return 0;
+}
+
+
+void split(char *string, const char *delimiter, char args[MAX_ARGS][MAX_LEN]) {
+	int i = 0;
+	char *token = strtok(string, delimiter);
+
+	while (token != NULL && i < MAX_ARGS) {
+		strncpy(args[i], token, MAX_LEN - 1);
+		args[i][MAX_LEN - 1] = '\0';
+		i++;
+		token = strtok(NULL, delimiter);
+	}
+
+	// mark end
+	if (i < MAX_ARGS)
+		args[i][0] = '\0';
+}
+
+
+
+int build_cmds(char args[MAX_ARGS][MAX_LEN],
+		char *cmds[MAX_CMDS][MAX_ARGS]) {
+	int cmd = 0, arg = 0;
+
+	for (int i = 0; args[i][0] != '\0'; i++) {
+		if (strcmp(args[i], "|") == 0) {
+			cmds[cmd][arg] = NULL;
+			cmd++;
+			arg = 0;
+		} else {
+			cmds[cmd][arg++] = args[i];
+		}
+	}
+
+	cmds[cmd][arg] = NULL;
+	return cmd + 1;
+}
+
+
+void exec_pipeline(char *cmds[MAX_CMDS][MAX_ARGS], int n_cmds) {
+	int fd[2];
+	int prev_fd = -1; // to keep track of read end of pipe , if true means last output is stored , read from this.
+
+	for (int i = 0; i < n_cmds; i++) {
+
+		if (i < n_cmds - 1)
+			pipe(fd);
+
+		int  pid = fork();
+
+		if (pid == 0) { // child
+
+			if (prev_fd != -1) {                     // for the input-redirection
+				dup2(prev_fd, STDIN_FILENO);
+				close(prev_fd);
+			}
+
+			if (i < n_cmds - 1) {                                       //for the output re-direction
+				dup2(fd[1], STDOUT_FILENO);
+				close(fd[0]);
+				close(fd[1]);
+			}
+
+			execvp(cmds[i][0], cmds[i]);
+			exit(1);
+		}
+
+		// parent
+		if (prev_fd != -1)
+			close(prev_fd);
+
+		if (i < n_cmds - 1) {
+			close(fd[1]);
+			prev_fd = fd[0];
+		}
+	}
+
+	for (int i = 0; i < n_cmds; i++)
+		wait(NULL);
 }
 
 char *parse_commandline_argument(char *string)
@@ -111,7 +203,10 @@ int main()
 		generating_output_to_file = to_check_if_the_string_contains_redirection_to_file(string , generating_output_to_file ,output_file_name);
 		trim_trailing_space(string);
 
-		if(string[0] == 'c' && string[1] == 'd' ) {
+
+		int string_contains_pipe = contains_pipe(string);
+
+		if(!string_contains_pipe && string[0] == 'c' && string[1] == 'd' ) {
 			char *temp_string = parse_commandline_argument(string);
 
 			struct stat sb;
@@ -121,10 +216,9 @@ int main()
 
 			chdir(temp_string);
 		}
-		else if (access(string , X_OK) != 0) {
+		else if (!string_contains_pipe && access(string , X_OK) != 0) {
 
 			const char *environment_path = getenv("PATH");
-			char *args[2];
 			pid_t pid;
 			char fullpath_of_directory[SIZE], *temporary_full_path;
 			int status;
@@ -161,11 +255,19 @@ int main()
 					printf("*************ls found at %s****************\n", fullpath_of_directory);
 					break;
 				}
-
 			}
 			if(!status)
 				printf("the %s not found\n", string);
+		}
+		else if ( string_contains_pipe ) {
 
+			char argument[MAX_ARGS][MAX_LEN];
+			char *cmds [MAX_CMDS][MAX_ARGS];
+
+			split(string , " ", argument);
+
+			int n_cmds = build_cmds(argument, cmds);
+			exec_pipeline(cmds, n_cmds);
 		}
 		else{
 			pid = fork();
@@ -178,12 +280,10 @@ int main()
 					if(fp == NULL)
 						printf("Error generating the output into a file\n");
 				}
-
 				execv(args[0] , args);
 			}
 			else if(pid > 0)
 				wait(NULL);
-
 		}
 	}
 }
